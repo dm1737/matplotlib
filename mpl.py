46328f3a03a2ca1139ea9ab45eb96b75e035a876
dm1737 <davidmartin1737@gmail.com>
2021-11-12 08:45:08 -0500

    Legend, title, label

--- mpl.py
+++ mpl.py
@@ -1,5 +1,20 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3],[5,7,4])
+x = [1,2,3]
+y = [5,7,4]
 
+x2 = [1,2,3]
+y2 = [10,14,12]
+
+# Plot x and y values, label creates legend
+plt.plot(x,y, label='Line 1')
+plt.plot(x2,y2, label='Line 2')
+
+# Assign names to x and y axes
+plt.xlabel('Plot Number')
+plt.ylabel('Important var')
+
+# Display graph with title and legend
+plt.title('Graph\nCheck it')
+plt.legend()
 plt.show()
